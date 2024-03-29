650eb75721d1d10011d4de8f9e0eef7154213b8f
Muhammad Sajjad <157211977+Sajjadsay@users.noreply.github.com>
2024-03-29 10:23:10 +0500

    project_02

---
+++ index.ts
@@ -0,0 +1,105 @@
+import inquirer from "inquirer";
+import {options} from './names.js';
+
+
+interface User {
+    id : number
+    pin : number
+    name : string
+    accountnumber : number
+    balance : number
+}
+
+const userfunc = () => {
+
+    const uname = options;
+    let randn = Math.floor(Math.random()*15);
+    return uname[randn];
+}
+
+const createUser = () => {
+
+    let users : User[] = [];
+
+    for(let i=1;i<=5;i++){
+        let user : User = {
+            id: i,
+            pin: 1000 + i,
+            name: userfunc(),
+            accountnumber: Math.floor(1000000*Math.random()*1000000),
+            balance: 100000*i
+        }
+        users.push(user);
+    }
+
+    return users;
+}
+
+const atmmachine = async(users: User[]) => {
+
+    const response = await inquirer.prompt({
+        type : "number",
+        message : "Please enter your PIN",
+        name : "pin"
+    })
+
+    const user = users.find(user => user.pin === response.pin)
+
+    if(user){
+        console.log("Welcome ",user.name);
+        atmfunc(user);
+    }
+    else{
+        console.log("Invalid PIN");
+    }
+
+}
+
+const atmfunc = async(user : User) => {
+
+    const ans = await inquirer.prompt({
+        type : "list",
+        name : "select",
+        choices : ["Withdraw","Balance","Exit"],
+        message : "Please select your choice"
+    })
+
+    if(ans.select === "Withdraw"){
+
+        const amount = await inquirer.prompt({
+            type : "number",
+            name : "amountwithdraw",
+            message : "Please enter your amount :"
+        })
+        
+        if(amount.amountwithdraw > user.balance){
+            console.log("Insufficient balance.");
+            console.log("Current balance :",user.balance);
+        }
+        else if(amount.amountwithdraw > 25000){
+            console.log("You cannot withdraw more than 'Twenty five thaousands'");
+        }
+        else{
+            user.balance = user.balance - amount.amountwithdraw;
+            console.log("Withdraw amount :",amount.amountwithdraw);
+            console.log("Current balance :",user.balance);
+        }
+    }
+
+    if(ans.select === "Balance"){
+        console.log("Current balance :",user.balance);
+    }
+
+    if(ans.select === "Exit"){
+        console.log("Thanks for using our service.");
+    }
+
+}
+
+let users = createUser();
+
+// atmmachine(users);
+
+
+console.log(users);
+
---
+++ names.ts
@@ -0,0 +1,17 @@
+export const options = [
+    "Muhammad Sajjad",
+    "Muhammad Talha",
+    "Imad Yousaf",
+    "Roohullah Khan",
+    "Attaullah",
+    "Muhammad Sohaib",
+    "Muhammad Junaid",
+    "Ijaz Khan",
+    "Adil Khan",
+    "Shahid Khan",
+    "Jamsheed Khan",
+    "Muhammad Mustafa",
+    "Maaz King",
+    "Azlan Khan",
+    "Wisal Ahmad Khan"
+]
